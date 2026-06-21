3126b165b2cb80ea15c6e6e7d5f16f492bf5d625
DjhonyOliveira <djonatandeoliveira@hotmail.com>
2026-06-21 12:01:19 -0300

    pathBackend

--- terraform/loadbalancer.tf
+++ terraform/loadbalancer.tf
@@ -32,5 +32,30 @@ resource "aws_lb_target_group_attachment" "worker_attachment" {
 
 output "load_balancer_dns" {
   value       = aws_lb.k3s_nlb.dns_name
-  description = "DNS do seu cluster para apontar seus domínios"
+  description = "DNS do cluster para apontar domínios"
+}
+
+resource "aws_lb_target_group" "backend_tg" {
+  name     = "k3s-backend-tg"
+  port     = 30080
+  protocol = "TCP"
+  vpc_id   = aws_vpc.k3s_vpc.id
+}
+
+resource "aws_lb_listener" "backend_listener" {
+  load_balancer_arn = aws_lb.k3s_nlb.arn
+  port              = 80
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.backend_tg.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "backend_worker_attachment" {
+  count            = 2
+  target_group_arn = aws_lb_target_group.backend_tg.arn
+  target_id        = aws_instance.k3s_worker[count.index].id
+  port             = 30080
 }
